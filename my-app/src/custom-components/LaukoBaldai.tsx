2a6e795b2b6a896260cbc54ef6f8db7d4c3ace35
Rimantas Preiksas <r.preiks@gmail.com>
2025-03-13 11:02:59 +0100

    bug

--- my-app/src/custom-components/LaukoBaldai.tsx
+++ my-app/src/custom-components/LaukoBaldai.tsx
@@ -39,9 +39,9 @@ const LaukoBaldai = () => {
         <Box>
             <NavBar category={categoryName} />
             <Box className="image-gallery" mt={"12px"}>
-                {/* {sodoNameliai.map((path, index) => (
+                {sodoNameliai.map((path, index) => (
                     <Image key={index} loading="lazy" maxW="500px" src={path} alt={"Sodo Namelis"} />
-                ))} */}
+                ))}
                 <Image src={laukoBaldas_1}></Image>
             </Box>
         </Box>
