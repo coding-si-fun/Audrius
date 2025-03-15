b1c074a7a0a3f011dbbbc68ea2cde5484510a317
Rimantas Preiksas <r.preiks@gmail.com>
2025-03-15 03:13:01 +0100

    test

--- my-app/src/custom-components/NavBar.tsx
+++ my-app/src/custom-components/NavBar.tsx
@@ -13,6 +13,7 @@ const NavBar = ({ category }: NavbarProps) => {
         <HStack justifyContent="space-between" px="12px">
             <Image src={logo} boxSize="64px" borderRadius="100%" />
             <Text fontSize={"2xl"}>{category}</Text> {/* Display formatted category name */}
+
             <ColorMode />
         </HStack>
     );
